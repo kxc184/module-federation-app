3269597eabc05e58eab0d166605d4b649df41977
Anthony M Formichelli <Anthony.M.Formichelli@Sherwin.com>
2024-06-24 15:34:27 -0400

    created order history component to be completed later. updated styles in header

--- packages/homepage/src/App.tsx
+++ packages/homepage/src/App.tsx
@@ -2,29 +2,34 @@ import "./App.css";
 import Logo from "../public/sherwinLogo.png";
 import MiniCart from "../public/minicart.svg";
 import SearchIcon from "../public/searchicon.svg";
+import OrderHistory from "./components/OrderHistory";
 
 function App() {
   return (
     <main className=" border border-solid mx-auto">
-      <nav className="container mx-auto ">
-        <ul className="flex gap-x-4 items-center  border border-solid text-xs">
+      <nav className="container mx-auto">
+        <ul className="px-20 flex gap-x-24 items-center border border-solid text-xs w-[75vw]">
           <li className=" p-4 border-r border-solid">
-            <p className="">ACCURA HEALTHCARE OF KNOXVILLE</p>
-            <p> Account: 2524-3595-3</p>
+            <p className="text-lg font-semibold">
+              ACCURA HEALTHCARE OF KNOXVILLE
+            </p>
+            <p className="text-base text-gray-500 font-medium">
+              Account: 2524-3595-3
+            </p>
           </li>
           <li className=" p-4 border-r border-solid">
-            <a href="#">
-              <u>My Store:</u>
-              <span>
+            <a className="text-lg" href="#">
+              <u>My Store: </u>
+              <span className="text-base text-gray-500 font-medium">
                 AMES <br /> #703966 <br /> (745) 202-1274 <br /> 7:00 AM - 5:00
                 PM
               </span>
             </a>
           </li>
-          <li className=" p-4 border-r border-solid text-center ">
+          <li className=" p-4 border-r border-solid text-center text-lg font-semibold">
             <a href="#">Quick Order</a>
           </li>
-          <li className=" p-4 border-r border-solid text-center">
+          <li className=" p-4 border-r border-solid text-center text-lg font-semibold">
             <a href="#">Order History</a>
           </li>
           <li className="ml-auto p-4 ">
@@ -35,20 +40,19 @@ function App() {
         </ul>
       </nav>
       <section className="h-[250px] bg-gradient-to-r from-zinc-800 via-zinc-600 to-zinc-800">
-        Header
         <div className="container mx-auto h-full">
-          <img className="px-8" src={Logo}></img>
+          <img className="px-8 py-5" src={Logo}></img>
         </div>
       </section>
-      <section className="container mx-auto bg-zinc-800 h-[964px] mt-[-100px] relative">
-        <ul className="flex items-center border-2 border-white h-14 sticky top-0 place-content-evenly">
+      <section className="container mx-auto h-[964px] mt-[-100px] relative">
+        <ul className="flex items-center  bg-zinc-800 h-[100px] sticky top-0 place-content-evenly z-50">
           <li className="px-10 bg-red-500">
             <h1 className="font-bold text-white text-xl ">SHOP BY CATEGORY</h1>
           </li>
           <li className="bg-white w-7/12 h-10 flex items-center rounded-lg">
             <input
               placeholder="What product would you like to order?"
-              className="w-full text-2xl px-4 outline-none "
+              className="w-full text-2xl px-4 outline-none"
             ></input>
             <button className=" w-10 h-8 flex flex justify-center  flex items-center">
               <img src={SearchIcon}></img>
@@ -61,16 +65,17 @@ function App() {
             </button>
           </li>
         </ul>
-        <section className="container mx-auto bg-red-500 h-5/6 w-9/12 absolute bottom-0 right-0">
+        <section className="container mx-auto bg-red-500 h-[865px] w-9/12 absolute bottom-0 right-0">
           <div className="h-full w-full relative none">
             <img
               className="absolute top-0 right-0 w-full"
               src="https://sherwin-ebus-ucm-production-s3.s3.us-east-2.amazonaws.com/content_images/store_image_punchout_atf.jpeg"
             ></img>
+            <OrderHistory />
           </div>
         </section>
       </section>
-      <footer className=" bg-[#e7e6e5] h-[300px]">Footer</footer>
+      <footer className=" bg-[#e7e6e5] h-[300px]"></footer>
     </main>
   );
 }
---
+++ packages/homepage/src/components/OrderHistory.tsx
@@ -0,0 +1,7 @@
+export default function OrderHistory() {
+  return (
+    <div className="container mx-auto h-1/4 w-400px absolute bottom-0 bg-blue-600">
+      <p>ORDER HISTORY</p>
+    </div>
+  );
+}
