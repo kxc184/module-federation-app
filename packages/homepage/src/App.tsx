725450c27b4bf01c4f52f49d5a3412958f25aa26
Anthony M Formichelli <Anthony.M.Formichelli@Sherwin.com>
2024-06-24 14:39:50 -0400

    store image +

--- packages/homepage/src/App.tsx
+++ packages/homepage/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Logo from "../public/sherwinLogo.png";
 import MiniCart from "../public/minicart.svg";
+import SearchIcon from "../public/searchicon.svg";
 
 function App() {
   return (
@@ -39,27 +40,35 @@ function App() {
           <img className="px-8" src={Logo}></img>
         </div>
       </section>
-      <section className="container mx-auto bg-zinc-800 h-[964px] m-[-100px]">
-        <nav className="container mx-auto">
-          <ul className="flex gap-x-3 items-center bg-blue-600">
-            <li className="px-10 bg-red-500">
-              <p>HASD</p>
-            </li>
-            <li className="bg-white w-7/12 inline-flex">
-              <input
-                placeholder="What product would you like to order?"
-                className="w-10/12 border-stone-950 border-2"
-              ></input>
-              <p>ICON</p>
-            </li>
-            <li>
-              <div>
-                <img src={MiniCart}></img>
-                <p>$0.00</p>
-              </div>
-            </li>
-          </ul>
-        </nav>
+      <section className="container mx-auto bg-zinc-800 h-[964px] mt-[-100px] relative">
+        <ul className="flex items-center border-2 border-white h-14 sticky top-0 place-content-evenly">
+          <li className="px-10 bg-red-500">
+            <h1 className="font-bold text-white text-xl ">SHOP BY CATEGORY</h1>
+          </li>
+          <li className="bg-white w-7/12 h-10 flex items-center rounded-lg">
+            <input
+              placeholder="What product would you like to order?"
+              className="w-full text-2xl px-4 outline-none "
+            ></input>
+            <button className=" w-10 h-8 flex flex justify-center  flex items-center">
+              <img src={SearchIcon}></img>
+            </button>
+          </li>
+          <li className="bg-white">
+            <button className="w-16 columns-1 flex">
+              <img src={MiniCart}></img>
+              <p>$0.00</p>
+            </button>
+          </li>
+        </ul>
+        <section className="container mx-auto bg-red-500 h-5/6 w-9/12 absolute bottom-0 right-0">
+          <div className="h-full w-full relative none">
+            <img
+              className="absolute top-0 right-0 w-full"
+              src="https://sherwin-ebus-ucm-production-s3.s3.us-east-2.amazonaws.com/content_images/store_image_punchout_atf.jpeg"
+            ></img>
+          </div>
+        </section>
       </section>
       <footer className=" bg-[#e7e6e5] h-[300px]">Footer</footer>
     </main>
