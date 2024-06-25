67f3b92d088b5195ca905ea4f81418295da60899
Anthony M Formichelli <Anthony.M.Formichelli@Sherwin.com>
2024-06-25 09:06:53 -0400

    order history component

--- packages/homepage/src/components/OrderHistory.tsx
+++ packages/homepage/src/components/OrderHistory.tsx
@@ -1,7 +1,57 @@
+import React, { useEffect, useState } from "react";
+
 export default function OrderHistory() {
+  const [historyLength, setHistoryLength] = useState("");
+  const [order, setOrder] = useState("");
+  const rand = Math.round(Math.random() * 100);
+  useEffect(() => {
+    console.log(historyLength);
+  }, [historyLength]);
   return (
-    <div className="container mx-auto h-1/4 w-400px absolute bottom-0 bg-blue-600">
-      <p>ORDER HISTORY</p>
+    <div className="container mx-auto h-1/4 w-400px absolute bottom-0 bg-white shadow-lg">
+      <div className="h-1/3 bg-gray-400 flex flex-row items-center shadow-md">
+        <p className="font-bold text-xl px-10 text-gray-700 w-56 ">
+          Order History
+        </p>
+        <select
+          onChange={(e) => {
+            setHistoryLength(e.target.value);
+          }}
+          className="w-72 h-3/6"
+        >
+          <option value="" disabled selected></option>
+          <option value="1mo">1 Month</option>
+          <option value="2mo">2 Months</option>
+          <option value="3mo">3 Months</option>
+          <option value="4mo">6 Months</option>
+          <option value="1yr">1 Year</option>
+          <option value=">1yr"> {">"} 1 Year</option>
+        </select>
+        <select
+          onChange={(e) => {
+            setOrder(e.target.value);
+          }}
+          disabled={historyLength === ""}
+          className="w-96 h-3/6 ml-12"
+        >
+          <option></option>
+          <option value={1}>Order 1</option>
+          <option value={2}>Order 2</option>
+          <option value={3}>Order 3</option>
+          <option value={4}>Order 4</option>
+        </select>
+        <button
+          disabled={historyLength === ""}
+          className="mx-5 w-48 bg-sky-500 disabled:bg-gray-200 text-lg font-semibold p-1 "
+        >
+          VIEW ORDER DETAIL
+        </button>
+      </div>
+      <div className="container mx-auto h-2/3 white flex items-end text-gray-600 ">
+        <p className="font-semibold text-xl ml-10 mb-8">
+          {order !== "" && rand + " Items in this order"}
+        </p>
+      </div>
     </div>
   );
 }
